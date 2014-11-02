b7f9a990bb8fab21bdbbe56eafafdc64c5bc3156
atsuo yamada <yamada-git@tir.jp>
2014-11-02 18:12:35 +0900

    Write descriptions and others

--- vnctst/template/posts/games/op0008.html.clj
+++ vnctst/template/posts/games/op0008.html.clj
@@ -1,10 +1,10 @@
 ; @layout  post
 ; @title   トミーパトロール
-; @comment ページ作成
+; @comment 0.1.1
 ; @date 2014/11/02
 ; @nocache true
 
-(def latest-version "0.1.0")
+(def latest-version "0.1.1")
 
 (defn heading-2 [title & [desc]]
   [:div
@@ -15,56 +15,49 @@
 
 (p [:a {:href "http://ahoge.info/15/index.php#39"} "あほげー第15回"]
    "エントリー作品「トミーパトロール」紹介ページです。")
-[:ul [:li "テーマは「トミー」。"]]
 
-(p "解説はまたあとで書きます。")
+[:ul
+ [:li "今回のあほげーのお題は「トミー」。トミーって何だ？"]
+ [:li
+  "結果的にタイトルにまでなってしまった個人的裏テーマは"
+  "『自分の中のうろおぼえのムーンパトロールの自機』。"
+  [:br]
+  "どうしてこうなった感が非常に強い。半日ぐらい悩んだ結果がこれだよ！"
+  "素直に、社名か人名にしとくべきだったか。"]]
 
 
 (heading-2 "Play" "遊ぶ")
 (p [:a {:href "/op0008/" :target "_blank"} "http://vnctst.tir.jp/op0008/"]
+   " "
    "にて遊べます。")
 
 
 (heading-2 "Staff" "製作者および利用素材など")
-(p "あとで書く")
-;[:dl
-; [:dt "ミドルウェア"]
-; [:dd (link "Unity" "http://japan.unity3d.com/unity/")]
-; [:dt "フォント"]
-; [:dd (link "mp1mm16tir" "https://github.com/ayamada/mplus-1mn-medium-16-fnt-tir")]
-; [:dt "ＢＧＭ"]
-; [:dd
-;  (link "ニコニコ自作ゲームフェス用ニコ素材"
-;        "http://commons.nicovideo.jp/user/upload/659862?t=audio")]
-; [:dt "利用させていただいた、他者作成の背景画像"]
-; [:dd
-;  (link "きまぐれアフター" "http://www5d.biglobe.ne.jp/~gakai/")
-;  "様の"
-;  (link "学校_教室_窓側" "http://www5d.biglobe.ne.jp/~gakai/")
-;  [:br]
-;  "(下手な加工：技情研ネット 山田)"
-;  ]
-; [:dt "利用させていただいた、他者作成のモンスター画像"]
-; [:dd
-;  (link "とり夫" "http://www.pixiv.net/member.php?id=5887541")
-;  "様の"
-;  (link "エアジャイアント" "http://www.pixiv.net/member_illust.php?mode=medium&illust_id=33106619")]
-; [:dt "利用させていただいた、他者作成の魚類画像"]
-; [:dd
-;  (link "魚類" "http://www.pixiv.net/member.php?id=698269")
-;  "様の"
-;  (link "中段突き" "http://www.pixiv.net/member_illust.php?mode=medium&illust_id=43763623")]
-; [:dt "利用させていただいた、他者作成のUnityPackage"]
-; [:dd "Unity標準アセットのSkybox"]
-; [:dd (link "Particle Cloud Package" "http://forum.unity3d.com/threads/particle-cloud-package.54833/") "の雲"]
-; [:dd (link "UnitySkyboxShaders" "https://github.com/keijiro/UnitySkyboxShaders")]
-; [:dt "企画、プログラミング、素材加工、残りグラフィック、効果音、その他いろいろ"]
-; [:dd "技情研ネット 山田"]
-; ]
+
+[:ul
+ [:li "めんどいので大部分のリンクは省略してます。必要ならぐぐってください。"]]
+
+[:dl
+ [:dt "ライブラリ類"]
+ [:dd "ClojureScript"]
+ [:dd "pixi.js"]
+ [:dd "webfont.js"]
+ [:dd "howler.js"]
+ [:dt "フォント"]
+ [:dd (link "M+ 1M bold" "http://mplus-fonts.sourceforge.jp/")]
+ [:dt "ＢＧＭ"]
+ [:dd
+  (link "ニコニコ自作ゲームフェス用ニコ素材"
+        "http://commons.nicovideo.jp/user/upload/659862?t=audio")]
+ [:dt "企画、プログラミング、グラフィック、効果音、その他いろいろ"]
+ [:dd "技情研ネット 山田"]
+ ]
 
 
 (heading-2 "History" "更新履歴")
 [:dl
+ [:dt "2014/11/02 version 0.1.1"]
+ [:dd "ボタンを押してもすぐにアクションが実行されない時があったので、ボタンを押したら即座にアクションを実行するように直した"]
  [:dt "2014/11/02 version 0.1.0 あほげー参加バージョン"]
  [:dd "公開"]
  [:dd "公式ページ設置"]
